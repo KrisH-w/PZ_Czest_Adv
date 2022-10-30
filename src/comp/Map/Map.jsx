ad86d9446e7cfcf3d77b606661d98a4316e528fb
Kris <krzysiekhab@gmail.com>
2022-10-30 18:40:04 +0100

    Changes, List Implementation

--- src/comp/Map/Map.jsx
+++ src/comp/Map/Map.jsx
@@ -1,8 +1,32 @@
 import React from "react";
+import GoogleMapReact from "google-map-react";
+import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined"
+import { Rating } from "@material-ui/lab";
+
+import useStyles from "./MapStyles";
+import { GoogleMap } from "@react-google-maps/api";
 
 const Map = () => {
-    return (
-        <h1>Map</h1>
-    )
+    const classes = useStyles();
+    const isMobile = useMediaQuery('(min-witdth: 600px');
+    const coords = {lat:50.807398327964584, lng:19.111692639912793};
+    return(
+        <div className={classes.mapContainer}>
+            <GoogleMapReact 
+                bootstrapURLKeys={{key: ''}}
+                defaultCenter={coords}
+                center={coords}
+                defaultZoom={12}
+                margin={[50,50,50,50]}
+                options={''}
+                onChange={''}
+                onChildClick={''}
+            >
+
+            </GoogleMapReact>
+        </div>
+    );
+    
 }
 export default Map;
